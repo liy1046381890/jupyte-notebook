948058206f035817938a0df8cc54e8864c1caaa0
八月 <liyang@syscloud.cn>
2019-03-15 17:27:36 +0800

    update

---
+++ titanic.py
@@ -0,0 +1,18 @@
+"""
+PassengerId 乘客id（int）
+Survived  存活（0，1）
+Pclass 票类别（1，2，3）
+Name 不作考虑
+Sex 性别（male，female）
+Age 年龄（float）缺省值按照Sex划分（平均，中位数，(max-min)/2+min ...)补全
+SibSp 平辈（int）
+Parch 长辈晚辈 （int）
+Ticket 票号（str）
+Fare 票价（float）
+Cbin 缺失严重不作考虑
+Embarked 登船港口（C，Q，S）
+"""
+
+# 特征  量纲 (x-min)/(max-min)
+
+
