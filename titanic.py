e779e1da5d18117a7ce6b7c0340f70d69d25c10c
八月 <liyang@syscloud.cn>
2019-04-08 17:13:45 +0800

    u

--- titanic.py
+++ titanic.py
@@ -17,4 +17,26 @@ Embarked 登船港口（C，Q，S）
 # One-hot Encoding 独热编码
 # 离群点？
 
-
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+train = pd.read_csv('titanic/train.csv')
+test = pd.read_csv('titanic/test.csv')
+print(test.loc[test.Fare.isnull()].Ticket)#3701
+lst = [str(i) for i in range(3000, 6000)]
+print(test.loc[test.Ticket.isin(lst)].Ticket)#3410,3470
+print(test.loc[test.Ticket.isin(lst)].Fare)#8.7125,7.8875
+# plt.rcParams['font.sans-serif'] = ['SimHei']
+# plt.rcParams['axes.unicode_minus'] = False
+# Embarked_C = train.Pclass[train.Embarked == 'C'].value_counts()
+# Embarked_Q = train.Pclass[train.Embarked == 'Q'].value_counts()
+# Embarked_S = train.Pclass[train.Embarked == 'S'].value_counts()
+# Pclass_df = pd.DataFrame({'C': Embarked_C, 'Q': Embarked_Q, 'S': Embarked_S})
+# Pclass_df.plot(kind='bar', stacked=True)
+# plt.title('不同票等乘客的登船港口分布')
+# plt.xlabel('票等')
+# plt.ylabel('乘客上传港口')
+# plt.show()
+# print(train.loc[train.Pclass.isin([1])].Embarked.mode()[0])
+print(train.loc[train.Sex.isin(['female'])].Age.mean())
